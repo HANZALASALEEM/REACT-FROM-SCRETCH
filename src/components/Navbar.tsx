4d2d7f334b2140e4df4a853ca02f46a0b117df06
HANZALASALEEM <hanzalabutt903@gmail.com>
2024-08-28 19:11:29 +0500

    Add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="h-14">
+      <ul className="h-full flex flex-row items-center justify-between px-4">
+        <li className="h-full flex items-center">
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? 'border-b-2 border-blue-600' : '')}>
+            Refactor React Code
+          </NavLink>
+        </li>
+        <li className="h-full flex items-center">
+          <NavLink
+            to="/performence"
+            className={({ isActive }) => (isActive ? 'border-b-2 border-blue-600' : '')}>
+            Performance in React
+          </NavLink>
+        </li>
+        <li className="h-full flex items-center">
+          <NavLink
+            to="/referential"
+            className={({ isActive }) => (isActive ? 'border-b-2 border-blue-600' : '')}>
+            Referential Equality
+          </NavLink>
+        </li>
+        <li className="h-full flex items-center">
+          <NavLink
+            to="/reduceRendering"
+            className={({ isActive }) => (isActive ? 'border-b-2 border-blue-600' : '')}>
+            Reduce Component Rerendering
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
