2e40263107a1abc806e7c21254b48711d7fe526c
HANZALASALEEM <hanzalabutt903@gmail.com>
2024-08-28 19:14:27 +0500

    Fix (Cannot GET /performence) on Refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const prod = process.env.NODE_ENV === "production";
+const prod = process.env.NODE_ENV === 'production';
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: prod ? "production" : "development",
-  entry: "./src/index.tsx",
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
   output: {
-    path: __dirname + "/dist/",
+    path: __dirname + '/dist/',
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -15,9 +16,9 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         resolve: {
-          extensions: [".ts", ".tsx", ".js", ".json"],
+          extensions: ['.ts', '.tsx', '.js', '.json']
         },
-        use: "ts-loader",
+        use: 'ts-loader'
       },
       //   {
       //     test: /\.css$/,
@@ -25,15 +26,18 @@ module.exports = {
       //   },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-    ],
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
   },
-  devtool: prod ? undefined : "source-map",
+  devServer: {
+    historyApiFallback: true
+  },
+  devtool: prod ? undefined : 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: 'index.html'
     }),
-    new MiniCssExtractPlugin(),
-  ],
+    new MiniCssExtractPlugin()
+  ]
 };
