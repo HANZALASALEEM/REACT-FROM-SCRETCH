b375ee8bc7df335babfe1533cf61227032ac13a1
HANZALASALEEM <hanzalabutt903@gmail.com>
2024-08-29 18:04:19 +0500

    tailwind.config.js is Treated as ES Module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+export const content = ['./index.html', './src/**/*.{js,jsx,ts,tsx}'];
+export const theme = {
+  extend: {}
 };
+export const plugins = [];
